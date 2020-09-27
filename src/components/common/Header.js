774c621ed27578fce5988e5f410bfda2693d1f44
MarkCalderon <mac.a4@live.com>
2020-09-27 20:43:02 +0800

    [edit] added missing header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,9 +5,28 @@ export default class Header extends React.Component {
         return (
             <header className="siteHeader">
                 <div className="siteHeader__inner">
-                    <nav>
-                        <a className="siteHeader__logo" href="#"></a>
-                    </nav>
+                    <div className="siteHeader__innerSite">
+                        <a href="./">
+                            <img className="siteHeader__innerSiteName" src="./../assets/img/common/portfolio-02.png" />
+                        </a>
+                    </div>
+                    <div className="siteHeader__innerDirectoryList flex__field">
+                        <div className="siteHeader__innerDirectoryListItem">
+                            <a href="https://twitter.com/markzev0" target="_blank">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                        </div>
+                        <div className="siteHeader__innerDirectoryListItem">
+                            <a href="https://github.com/MarkCalderon" target="_blank">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </div>
+                        <div className="siteHeader__innerDirectoryListItem">
+                            <a href="https://www.linkedin.com/in/markcalderon/" target="_blank">
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </header>
         )
