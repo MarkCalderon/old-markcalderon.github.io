320f4b5cb47d819d8c1d6079aeb204ae88c62514
Mark Calderon <mark_calderon@commude.ph>
2020-11-19 22:35:14 +0800

    [edit] added additional cors header fix and react fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -14,13 +16,13 @@ module.exports = {
     ],
     // Set the build output directory
     output: {
-        path: path.resolve(__dirname, 'dist/assets'),
+        path: path.resolve(__dirname, './dist/'),
         // Webpack will always output a js.
-        filename: 'js/[name].js'
+        filename: 'assets/js/[name].js'
     },
     devServer: {
         // Set where the server will serve assets from/similar to public_html/.
-        contentBase: path.resolve(__dirname, 'dist'),
+        contentBase: path.resolve(__dirname, './dist/'),
         // Compress assets using gzip compression.
         compress: true,
         // Show logs on Dev Tools.
@@ -34,10 +36,62 @@ module.exports = {
         // Sets where the assets are served from.
         publicPath: '/assets/',
         // Server Port
-        port: 9000
+        port: 9000,
+        // Allows pages to load exactly when routing to multiple pages
+        historyApiFallback: true,
+        watchOptions: { 
+            aggregateTimeout: 300,
+            poll: 1000 
+        },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/pug/index.pug',
+        }),
+        new HtmlBeautifyPlugin({
+            config: {
+                html: {
+                    end_with_newline: true,
+                    indent_size: 2,
+                    indent_with_tabs: true,
+                    indent_inner_html: true,
+                    preserve_newlines: true,
+                    unformatted: ['p', 'i', 'b', 'span']
+                }
+            },
+            replace: [' type="text/javascript"']
+        }),
+        // Extracts the main.css
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[name].css',
+            chunkFilename: '[id].css',
+        }),
+        // Copies the Image folder to destination
+        new CopyPlugin({
+            patterns: [
+                { 
+                    from: path.resolve(__dirname, './src/assets/img'), 
+                    to: path.resolve(__dirname, './dist/assets/img'),
+                },
+            ],
+            options: {
+                concurrency: 100,
+            },
+        }),
+    ],
     module: {
         rules: [
+            { 
+                // Handles the processing of PUG files
+                test: /\.pug$/,
+                use: ["pug-loader"]
+            },
             {
                 // Handles the processing of SASS/SCSS files
                 test: /\.scss$/,
@@ -106,22 +160,4 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-        // Extracts the main.css
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-            chunkFilename: '[id].css',
-        }),
-        new CopyPlugin({
-            patterns: [
-                { 
-                    from: path.resolve(__dirname, 'src/assets/img'), 
-                    to: path.resolve(__dirname, 'dist/assets/img'),
-                },
-            ],
-            options: {
-                concurrency: 100,
-            },
-        }),
-    ],
 }
